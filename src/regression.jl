2fe13d9603036601942a86081d3109551ab17265
Yueh-Hua Tu <a504082002@gmail.com>
2021-04-09 16:03:26 +0800

    refactor regression input X

--- src/regression.jl
+++ src/regression.jl
@@ -10,17 +10,17 @@ mutable struct LinearRegression{T<:Real,V<:AbstractVector}
     se::V
 end
 
-LinearRegression(d::Integer) = LinearRegression(rand(d+1), rand(),rand(d+1))
+LinearRegression(d::Integer) = LinearRegression(rand(d+1), rand(), rand(d+1))
 
 coef(model::LinearRegression) = model.β
 std(model::LinearRegression) = model.σ
 stderror(model::LinearRegression) = model.se
 
-# X ∈ (feat * obs)
-design_matrix(x::AbstractVector{T}) where {T} = vcat(ones(T, 1, length(x)), x')
-design_matrix(X::AbstractMatrix{T}) where {T} = vcat(ones(T, 1, size(X, 2)), X)
+# X ∈ (obs × feat)
+design_matrix(x::AbstractVector{T}) where {T} = hcat(ones(T, length(x)), x)
+design_matrix(X::AbstractMatrix{T}) where {T} = hcat(ones(T, size(X, 1)), X)
 
-predict(model::LinearRegression, X::AbstractVecOrMat) = design_matrix(X)'*model.β
+predict(model::LinearRegression, X::AbstractVecOrMat) = design_matrix(X)*model.β
 
 residual(model::LinearRegression, X::AbstractVecOrMat, y::AbstractVector) = y - predict(model, X)
 
@@ -29,16 +29,16 @@ MSE(model::LinearRegression, X::AbstractVecOrMat, y::AbstractVector) = SSE(model
 
 function fit!(model::LinearRegression, X::AbstractVecOrMat, y::AbstractVector{T}) where {T}
     D = design_matrix(X)
-    model.β = inv(D*D') * D*y
+    model.β = inv(D'*D) * D'*y
     r = residual(model, X, y)
     model.σ = sqrt(r'*r / length(r))
     # se of slopes
     n = length(y)
-    X̄ = mean(X, dims=2)
+    X̄ = mean(X, dims=1)
     ϵₓ = X .- X̄
-    SS = sum(x -> x.^2, ϵₓ, dims=2)
+    SS = sum(x -> x.^2, ϵₓ, dims=1)
     mse = MSE(model, X, y)
-    se_slope = sqrt.(mse ./ SS)
+    se_slope = sqrt.(mse ./ vec(SS))
     # se of intercept
     X̄ = [one(T), (X̄.^2)...]
     SS = [n, SS...]
@@ -53,7 +53,7 @@ function fit(::Type{LinearRegression}, x::AbstractVector, y::AbstractVector)
 end
 
 function fit(::Type{LinearRegression}, X::AbstractMatrix, y::AbstractVector)
-    model = LinearRegression(size(X, 1))
+    model = LinearRegression(size(X, 2))
     fit!(model, X, y)
 end
 
